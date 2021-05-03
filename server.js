5117f1f3cec4e1bbdb7c4752e7dbf75221cca84f
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-05-03 00:44:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-// node js
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const SERVER = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, 'client', req.url === '/' ? 'index.html' : req.url + '.html')
+    fs.readFile(filePath, (err, content) => {
+        if(req.url === '/favicon.ico'){
+            return;
+        }
+        if(err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(content, 'utf8');
+    })
+})
+
+// const PORT = process.env.PORT || 5000;
+
+SERVER.listen(5001, () => console.log('Server is running...'));
