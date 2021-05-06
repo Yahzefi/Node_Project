b845c56405fd1807c31e35258751d77c060aebcf
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-05-06 00:09:29 -0400

    Add files via upload

---
+++ API/members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: "John Smith",
+        email: "johnsmith@gmail.com",
+        status: "active"
+    },
+    {
+        id: 2,
+        name: "Jane Smith",
+        email: "janesmith@gmail.com",
+        status: "active"
+    },
+    {
+        id: 3,
+        name: "Joe Smith",
+        email: "joesmith@gmail.com",
+        status: "active"
+    }
+]
+
+module.exports = members;
