5595b6f1b682ae2a349bfd7bea3340bb9e083a39
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-05-05 20:53:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,13 @@
-const http = require('http');
+const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
-const SERVER = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, 'client', 
-    req.url === '/' ? 'index.html' 
-    : req.url === '/favicon.ico' ? 'favicon.ico' 
-    : req.url)
-    fs.readFile(filePath, (err, content) => {
-        if(err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end(content, 'utf8');
-    })
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'))
 })
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-SERVER.listen(5000, () => console.log('Server is running...'));
-
-function changeFilePath(currentDir, newDir) {
-    console.log("Hello");
-}
-
-module.exports = changeFilePath(); // can I even use a function as a module export?
+app.listen(PORT, () => console.log("Server now running!"))
