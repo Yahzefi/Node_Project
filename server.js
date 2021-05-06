398a53caf6f8febc1a1f169bb20fa139af6bad04
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-05-06 00:08:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
+const members = require('./API/members');
 
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'index.html'))
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'index.html'))
+// })
+
+app.get('/api/members', (req, res) => {
+    res.json(members);
 })
+
+app.use(express.static(path.join(__dirname, 'client')));
 
 const PORT = process.env.PORT || 5000
 
