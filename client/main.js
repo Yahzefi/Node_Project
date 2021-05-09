37f4a39f7f5decd8dfc4a3db18e3574cefc854a9
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-05-08 20:00:56 -0400

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,5 +1,11 @@
+import { DOMElement } from '../Refactorization/DOMElement';
+
+let memberBox = new DOMElement('div', 'member_container', 'mem-cont');
+
+document.body.appendChild(memberBox.generateElement());
+
 fetch("/api/members")
 .then((data)=>data.json())
 .then((data)=> {
-    document.getElementById('fetchedName').textContent = data[0].name;
+    console.log(data);
 });
